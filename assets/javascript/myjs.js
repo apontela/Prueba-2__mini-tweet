f178b759d9e606792c5b92468c02529e0a0470bb
Adriel <aponte96.la@gmail.com>
2018-07-26 21:07:38 -0400

    Prueba Terminada

--- assets/javascript/myjs.js
+++ assets/javascript/myjs.js
@@ -26,24 +26,24 @@ $(function () {
         let source = ($(".imagen-de-usuario").attr("src"));
         $(".imagen-de-usuario__tweet").attr("src",source);
     })
-    let counter = 0;
     $(".publicar-tweet").submit(function (e) {
         e.preventDefault();
-        $(".plantilla-publicacion").css("display", "block");
+        $(".plantilla-publicacion:last-child").clone().css("display", "block").prependTo(".main__tweets").hide().fadeIn(300);
         let texto_tweet = $("#texto-tweet").val();
-        $(".publicacion__text p").text(texto_tweet);
+        $(".plantilla-publicacion:first-child .publicacion__text p").text(texto_tweet);
     })
-    $(".fa-heart").on("click", function () {
-        $(".fa-heart").css("color", "#EE2E31");
+    $(".main__tweets").on("click", ".fa-heart", this, function () {
+        let counter = parseInt($(this).parent().find("small").text());
+        $(this).css("color", "#EE2E31");
         counter = counter + 1;
-        $(".publicacion__social small").text(counter);
+        $(this).parent().find("small").text(counter);
     })
-    $(".fa-trash").on("click", function () {
+    $(".main__tweets").on("click", ".fa-trash", this, function () {
         let confirma = confirm("¿Estás Seguro de Eliminar Éste Mensaje?");
         if (confirma == false) {
             return
         }
-        $(".fa-trash").parent().parent().remove();
+        $(this).parent().parent().remove();
     })
     
 })
